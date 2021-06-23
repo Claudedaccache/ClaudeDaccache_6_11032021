cbf82b1297171dfa2b51b09009b90ac14611b975
Claude Daccache <claude.daccache@outlook.com>
2021-06-23 16:42:17 +0200

    modified Js

--- Js/Fetch.js
+++ Js/Fetch.js
@@ -1,10 +1,10 @@
-fetch("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json")
-.then(response => {
-   return response.json();
-}).then(data => console.log(data)) 
-.catch(function (err) {
-    console.log("Fetch problem show: " + err.message);
-  });
+// fetch("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json")
+// .then(response => {
+//    return response.json();
+// }).then(data => console.log(data)) 
+// .catch(function (err) {
+//     console.log("Fetch problem show: " + err.message);
+//   });
 
 
 // fetch("./index.json")
@@ -15,3 +15,12 @@ fetch("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-En
 // .catch(function (err) {
 //     console.log("Fetch problem show: " + err.message);
 //   });
+
+
+async function dataCollection (){
+const response = await fetch("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeData.json");
+const data = await response.json()
+console.log (data)
+}
+
+dataCollection()
