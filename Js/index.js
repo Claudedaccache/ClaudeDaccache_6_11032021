45690749be93e4a7c9cc939a545480f12954a9e7
Claude Daccache <claude.daccache@outlook.com>
2021-06-28 17:06:03 +0200

    adding and grouping the Js files in Js

--- Js/index.js
+++ Js/index.js
@@ -1,60 +1,24 @@
-
 // scrolling btn
 var scrollBtn = document.querySelector("#scrollMainPage");
 
 window.addEventListener("scroll", () => {
   const scrolled = window.scrollY;
 
-  console.log(scrolled)
+  // console.log(scrolled)
 
- if(scrolled < 300){
-    scrollBtn.style.display ="none";
+  if (scrolled < 300) {
+    scrollBtn.style.display = "none";
+  } else {
+    scrollBtn.style.display = "block";
+    scrollBtn.style.position = "fixed";
   }
-  else {
-    scrollBtn.style.display ="block";
-    scrollBtn.style.position ="fixed";}
-  }
-)
-
-// filter section:                                                working but need to make it sort and filter
-
-var FilterIcon = document.querySelector("#filterIcon");
-var FilterNotActive = document.querySelectorAll(".notActive")
-
-// filterIcon.forEach((btn) => btn.addEventListener("click", launchFilter));
-FilterIcon.addEventListener("click", launchFilter);
-
-function launchFilter () {
-  FilterNotActive.forEach((btn) => btn.classList.toggle('responsive'));
-
-  }
-
-
-// clickable hearts                                   working
-
-  var icons = document.querySelectorAll(".LikesIcon i");
-
-  icons.forEach((icon) => icon.addEventListener("click",  () => {
-  icon.classList.toggle('resp');
-  }))
-
-
-
-
-
-
-
-
-
-
-
+});
 
 // increasing and decreasing likes:                            not workingggg
 
 var likesNumber = document.querySelectorAll(".likesNumber");
 
 // icons.forEach((icon) => icon.addEventListener("click",  () => {
-
 
 // if(icon.className === "LikesIcon i.resp"){
 //   parseInt(likesNumber++)
@@ -65,11 +29,8 @@ var likesNumber = document.querySelectorAll(".likesNumber");
 
 // }))
 
-
-
 // for (i=0; i < icons.length; i++){
 //   icons[i].addEventListener("click", () => {
-
 
 //   if(icons.checked == true ){
 //       // parseInt(likesNumber++)
@@ -82,16 +43,6 @@ var likesNumber = document.querySelectorAll(".likesNumber");
 //   }
 // })}
 
-
-
-
-
-
-
-
-
-
-
 // total likes:
 
 // var totalLikes = document.querySelector("photographerLikesSec imput")
@@ -101,34 +52,7 @@ var likesNumber = document.querySelectorAll(".likesNumber");
 //   sum+=likeValue
 // })
 
-
 //   console.log(sum)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // opening of filter menu:
 // filterIcon.addEventListener("click", () => {
@@ -138,5 +62,3 @@ var likesNumber = document.querySelectorAll(".likesNumber");
 //     filterNotActive.className = "notActive";
 //   }
 // })
-
-
