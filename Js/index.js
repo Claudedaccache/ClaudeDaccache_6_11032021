22fca8cf568096fbc08c95dfe9ed2b0c3ef269b4
Claude Daccache <claude.daccache@outlook.com>
2021-07-27 09:51:15 +0200

    removed comments

--- Js/index.js
+++ Js/index.js
@@ -2,14 +2,10 @@ import data from "./../data.js";
 
 console.log(data);
 
-// scrolling btn
 var scrollBtn = document.querySelector("#scrollMainPage");
 
 window.addEventListener("scroll", () => {
   const scrolled = window.scrollY;
-
-  // console.log(scrolled)
-
   if (scrolled < 300) {
     scrollBtn.style.display = "none";
   } else {
